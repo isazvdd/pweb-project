5f6c3664e554dae60d7f024123ff124ce2cd24ee
Isadora Azevedo <isadora.azevedo.700@ufrn.edu.br>
2022-12-18 09:45:34 -0300

    functions

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,7 +6,7 @@ import { Error } from "../../components/Error";
 import { Header } from "../../components/Header";
 import styles from "../../styles/Home.module.css";
 
-export default function Home({ data, show }) {
+export function Home({ data, show }) {
   if (!show) return <div></div>;
 
   if (data.Error) {
