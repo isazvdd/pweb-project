534171277564676c105aeb71e2e0000345c41410
Isadora Azevedo <isadora.azevedo.700@ufrn.edu.br>
2022-12-19 15:13:56 -0300

    add new functions

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Spin } from "antd";
+import { Spin, Popover, Button } from "antd";
 import Link from "next/link";
 
 import { Error } from "../../components/Error";
 import { Header } from "../../components/Header";
 import styles from "../../styles/Home.module.css";
+import { Back } from "../Back";
 
 export function Home({ data, show }) {
   if (!show) return <div></div>;
@@ -24,9 +25,41 @@ export function Home({ data, show }) {
         <h2 className={styles.title}> Todas as cartas:</h2>
         <div className={styles.allCards}>
           {data.map((m) => (
-            <Link href={"../Card/" + m.cardnumber}>
-              <img src={m.image_url} className={styles.images} />
-            </Link>
+            <Popover
+              // className={styles.informations}
+
+              title="Informações da carta"
+              content={(content) => {
+                return (content = (
+                  <ul>
+                    <li>
+                      <span>Número da carta</span>
+                      <p>{m.cardnumber}</p>
+                    </li>
+
+                    <li>
+                      <span>Tipo de carta</span>
+                      <p>{m.type}</p>
+                    </li>
+
+                    <li>
+                      <Button onClick={() => m.cardnumber}>
+                        <Link href={"../Card/" + m.cardnumber}>
+                          {" "}
+                          Mais informações
+                        </Link>
+                      </Button>
+                    </li>
+                  </ul>
+                ));
+              }}
+              placement="top"
+              trigger="hover"
+            >
+              <Link href={"../Card/" + m.cardnumber}>
+                <img src={m.image_url} className={styles.images} />
+              </Link>
+            </Popover>
           ))}
         </div>
       </section>
